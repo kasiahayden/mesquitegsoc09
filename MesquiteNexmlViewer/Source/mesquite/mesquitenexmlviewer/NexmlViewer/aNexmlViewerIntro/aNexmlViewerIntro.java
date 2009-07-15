9502c8b8eee96a66b7a8807ce0b7ef524bfe9709
kasia.gsoc09 <kasia.gsoc09@0961ccb8-4ac7-11de-988a-a513c5287034>
2009-07-15 17:11:37 +0000

    Retrying adding aNexmlViewerIntro

---
+++ MesquiteNexmlViewer/Source/mesquite/mesquitenexmlviewer/NexmlViewer/aNexmlViewerIntro/aNexmlViewerIntro.java
@@ -0,0 +1,41 @@
+package mesquite.mesquitenexmlviewer.NexmlViewer.aNexmlViewerIntro;
+
+import mesquite.lib.duties.PackageIntro;
+
+public class aNexmlViewerIntro extends PackageIntro {
+	/*.................................................................................................................*/
+	public boolean startJob(String arguments, Object condition, boolean hiredByName) {
+ 		return true;
+  	 }
+  	 public Class getDutyClass(){
+  	 	return aNexmlViewerIntro.class;
+  	 }
+	/*.................................................................................................................*/
+    	 public String getExplanation() {
+		return "Introduces NexmlViewer.";
+   	 }
+   
+	/*.................................................................................................................*/
+    	 public String getName() {
+		return "NexmlViewer Introduction";
+   	 }
+	/*.................................................................................................................*/
+	/** Returns the name of the package of modules (e.g., "Basic Mesquite Package", "Rhetenor")*/
+ 	public String getPackageName(){
+ 		return "NexmlViewer";
+ 	}
+	/*.................................................................................................................*/
+	/** Returns information about a package of modules*/
+ 	public String getPackageCitation(){
+ 		return "NexmlViewer";
+ 	}
+	/** Returns whether there is a splash banner*/
+	public boolean hasSplash(){
+ 		return true; 
+	}
+	
+	public boolean getHideable(){
+		return false;
+	}
+
+}
